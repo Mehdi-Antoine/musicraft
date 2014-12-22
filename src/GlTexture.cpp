682daadf38898c352c55cc7dda493e34b85dbe5f
Mehdi-Antoine <mehdi.mahfoudi@gmail.com>
2014-12-22 15:12:22 +0100

    correction GlTexture

--- src/GlTexture.cpp
+++ src/GlTexture.cpp
@@ -1,1 +1,101 @@
+#include <GL/glew.h>
+#include <iostream>
+
+#include <glimac/FilePath.hpp>
+#include <glimac/Image.hpp>
+
 #include "include/GlTexture.hpp"
+
+using namespace glimac;
+
+//--------------------------------------------------------------------------------------------------------------
+//------------------------------------------------CONSTRUCTOR---------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------
+
+GlTexture::GlTexture(){
+	m_texture_data = NULL;
+	m_texture_id = 0;
+}
+
+GlTexture::GlTexture(FilePath path){
+
+    m_texture_data = loadImage(path);
+
+    if(m_texture_data == NULL){
+        std::cerr << "Pas de fichier image chargé pour la texture : " << path;
+    }
+
+
+    //CREATION DES VARIABLES OPENGL
+    glGenTextures(1, &m_texture_id);
+
+    //LOAD DES TEXTURES DANS OPEN GL
+    glBindTexture(GL_TEXTURE_2D, m_texture_id);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    glTexImage2D(GL_TEXTURE_2D,  
+                 0,  
+                  GL_RGBA,  
+                  m_texture_data->getWidth(),  
+                  m_texture_data->getHeight(),  
+                  0,  
+                  GL_RGBA,  
+                  GL_FLOAT,  
+                  m_texture_data->getPixels());
+
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+}
+
+//--------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------FUNCTIONS----------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------
+
+void GlTexture::load(FilePath path){
+	if(m_texture_data == NULL){
+
+		m_texture_data = loadImage(path);
+
+	    if(m_texture_data == NULL){
+	        std::cerr << "Pas de fichier image chargé pour la texture : " << path;
+	    }
+
+	    //CREATION DES VARIABLES OPENGL
+	    glGenTextures(1, &m_texture_id);
+
+	    //LOAD DES TEXTURES DANS OPEN GL
+	    glBindTexture(GL_TEXTURE_2D, m_texture_id);
+
+	    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	    glTexImage2D(GL_TEXTURE_2D,  
+	                 0,  
+	                  GL_RGBA,  
+	                  m_texture_data->getWidth(),  
+	                  m_texture_data->getHeight(),  
+	                  0,  
+	                  GL_RGBA,  
+	                  GL_FLOAT,  
+	                  m_texture_data->getPixels());
+
+	    glBindTexture(GL_TEXTURE_2D, 0);
+
+	}
+}
+
+void GlTexture::use(){
+
+	glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, m_texture_id);
+
+}
+
+void GlTexture::stopUse(){
+
+	glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+}
--- src/include/GlTexture.hpp
+++ src/include/GlTexture.hpp
@@ -1,17 +1,34 @@
 #pragma once
 
+#include <GL/glew.h>
+
 #include <glimac/FilePath.hpp>
+#include <glimac/Image.hpp>
+
+using namespace glimac;
 
 class GlTexture{
 private:
 
-	
+	std::unique_ptr<Image> m_texture_data;
+
+	GLuint m_texture_id;
 	
 public:
+
+//------------------------------------------------CONSTRUCTOR---------------------------------------------------
+
 	GlTexture();
-	GlTexture(glimac::FilePath dir_path);
+	GlTexture(glimac::FilePath path);
 
 	~GlTexture();
 
-	/* data */
+//-------------------------------------------------FUNCTIONS----------------------------------------------------
+
+	void load(glimac::FilePath path);
+
+	void use();
+
+	void stopUse();
+
 };
