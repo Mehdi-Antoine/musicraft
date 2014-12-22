f9b9b94d29ef81beb4781e085c70d4222c564e1e
Hisuuu <hp.hisu@gmail.com>
2014-12-22 17:12:54 +0100

    GlUniform

--- src/GlUniform.cpp
+++ src/GlUniform.cpp
@@ -1,1 +1,10 @@
 #include "include/GlUniform.hpp"
+
+GlUniform::GlUniform(GLuint gl_id, const std::string & name){
+	m_name = name;
+	m_value = glGetUniformLocation(gl_id, m_name);
+}
+
+GlUniform::~GlUniform(){
+	
+}
--- src/include/GlUniform.hpp
+++ src/include/GlUniform.hpp
@@ -1,12 +1,15 @@
 #pragma once
+#include <GL/glew.h>
+#include <string>
 
 class GlUniform{
 private:
-
+	std::string m_name;
+	GLint m_value;
 	
 	
 public:
-	GlUniform();
+	GlUniform(GLuint gl_id, const std::string & name);
 	~GlUniform();
 
 	/* data */
