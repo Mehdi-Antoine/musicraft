7a2ec8cc7a7fb7fdeb08b1d5b55024081b45f9c9
MAHFOUDI MEHDI-ANTOINE <mmahfo01@localhost.localdomain>
2015-01-08 12:58:48 +0100

    je sais pas trop ce que je fais

---
+++ src/Chunk.cpp
@@ -0,0 +1,128 @@
+#include "include/Chunk.hpp"
+#include <iostream>
+
+
+const int Chunk::m_size = SIZE;
+
+//--------------------------------------------------------------------------------------------------------------
+//------------------------------------------------CONSTRUCTOR---------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------
+
+Chunk::Chunk(char cube_type){
+
+	for (int x = 0; x < SIZE; ++x)
+	{
+		for (int y = 0; y < SIZE; ++y)
+		{
+			for (int z = 0; z < SIZE; ++z)
+			{		
+				m_chunk[x][y][z] = cube_type;				
+			}
+		}
+	}
+
+}
+
+Chunk::Chunk(const Chunk &chunk){
+
+	m_world_position = chunk.getWorldPosition();
+
+	for (int x = 0; x < SIZE; ++x)
+	{
+		for (int y = 0; y < SIZE; ++y)
+		{
+			for (int z = 0; z < SIZE; ++z)
+			{		
+				m_chunk[x][y][z] = chunk.getCubeType(x, y, z);				
+			}
+		}
+	} 
+
+}
+
+Chunk::~Chunk(){
+
+}
+
+//--------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------GETTERS-----------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------
+
+char Chunk::getCubeType(float x, float y, float z) const{
+
+	return m_chunk[getIndex(x)][getIndex(y)][getIndex(z)];
+}
+
+char Chunk::getCubeType(const glm::vec3 &position) const{
+	//std::cout << getIndex(position.x) << " " << getIndex(position.y) << " " << getIndex(position.z) << std::endl;
+	return m_chunk[getIndex(position.x)]
+				  [getIndex(position.y)]
+				  [getIndex(position.z)];
+
+}
+
+glm::vec3 Chunk::getWorldPosition() const{
+	return m_world_position;
+}
+
+//--------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------SETTERS-----------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------	
+
+void Chunk::setCubeType(int x, int y, int z, char cube_type){
+
+	m_chunk[getIndex(x)][getIndex(y)][getIndex(z)] = cube_type;
+
+}
+
+void Chunk::setCubeType(const glm::vec3 &position, char cube_type){
+
+	m_chunk[getIndex(position.x)]
+		   [getIndex(position.y)]
+		   [getIndex(position.z)] = cube_type;
+
+}
+
+void Chunk::setWorldPosition(const glm::vec3 world_position){
+	m_world_position = world_position;
+}
+
+//-------------------------------------------------FUNCTIONS----------------------------------------------------
+
+int Chunk::getIndex(float value) const{
+
+
+	float decimal = value - int(value);
+	return round(value);
+}
+
+glm::vec3 Chunk::getColorFromType(char cube_type){
+	switch (cube_type) {
+
+		case EMPTY:
+		  return glm::vec3(0,0,0);
+		  break;
+
+		case BASIC1:
+		  return glm::vec3(0.75,
+						   0.17,
+						   0.62);
+		  break;
+
+		case BASIC2:
+		  return glm::vec3(0.17,
+						   0.75,
+						   0.47);
+		  break;
+
+		case BASIC3:
+		  return glm::vec3(0.22,
+						   0.4,
+						   0.75);
+		  break;
+
+		default:
+		  return glm::vec3(1,1,1);
+		  break;
+	}
+}
